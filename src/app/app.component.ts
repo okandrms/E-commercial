88a5cae88222696f92c6bacab1b66fedcdd1fa13
okandrms <okanndrms@gmail.com>
2024-01-11 11:23:08 +0100

    Navbar and Home Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatIconModule, SlickCarouselModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Eindproef';
+  searchText: string = "";
+
+search() {
+  // perform the search here
+  console.log(this.searchText);
+}
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon'
 
 import { routes } from './app.routes';
 
