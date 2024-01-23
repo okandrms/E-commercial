b37e3f8b9bf5435838ff583690f130ab2988ccc1
Ruscav <rubenscavone@hotmail.com>
2024-01-23 14:44:57 +0100

    Toaster fix, adding teams database Premier League

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon'
 import { routes } from './app.routes';
 import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),provideToastr({positionClass: 'toast-bottom-right'})]
+  
+  providers: [
+    provideAnimations(), // required animations providers
+    provideToastr({positionClass: 'toast-bottom-right'}), // Toastr providers
+    provideRouter(routes)
+  ]
 };
